2d349e3a2e84afca489ad7786f96f2f2e716329e
Abbasov Ceyhun <abbasovceyhunn@hotmail.com>
2020-04-13 10:49:00 +0400

    add GET and POST method to routing

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 
 	router.HandleFunc("/", Index)
 
-	router.HandleFunc("/GetById/{id}", GetByID)
-	router.HandleFunc("/GetByWeight/{weight}", GetByWeight)
-	router.HandleFunc("/GetByHeight/{height}", GetByHeight)
+	router.HandleFunc("/GetById/{id}", GetByID).Methods("GET")
+	router.HandleFunc("/GetByWeight/{weight}", GetByWeight).Methods("GET")
+	router.HandleFunc("/GetByHeight/{height}", GetByHeight).Methods("GET")
 
-	router.HandleFunc("/AddPerson", AddPerson)
-	router.HandleFunc("/EditPerson", EditPerson)
-	router.HandleFunc("/RemovePerson/{id}", RemovePerson)
+	router.HandleFunc("/AddPerson", AddPerson).Methods("POST")
+	router.HandleFunc("/EditPerson", EditPerson).Methods("POST")
+	router.HandleFunc("/RemovePerson/{id}", RemovePerson).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
